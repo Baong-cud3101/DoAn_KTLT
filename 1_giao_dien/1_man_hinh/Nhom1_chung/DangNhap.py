07f1c0127f82f9d1bcf4601f1a5375777e317e3b
phucnmk25406h-lgtm <phucnmk25406h@st.uel.edu.vn>
2026-03-06 10:29:14 +0700

    DangNhap.py

--- 1_giao_dien/1_man_hinh/Nhom1_chung/DangNhap.py
+++ 1_giao_dien/1_man_hinh/Nhom1_chung/DangNhap.py
@@ -1,35 +1,40 @@
 import tkinter as tk
-from Nhom1_chung.Trangchu import TrangChuUI
+from tkinter import messagebox
+import json
+from TrangChu import TrangChuUI
+from DangKy import DangKyUI
 
 class DangNhapUI:
-    def __init__(self, root):
-        self.root = root
-        frame = tk.Frame(root)
+    def __init__(self,root):
+        self.root=root
+        frame=tk.Frame(root)
         frame.pack(fill="both",expand=True)
-        left = tk.Frame(frame,bg="#1cc5d8",width=450)
+        left=tk.Frame(frame,bg="#1cc5d8",width=400)
         left.pack(side="left",fill="y")
+
         tk.Label(
             left,
-            text="LOGO",
-            bg="#1cc5d8",
+            text="APP",
+            font=("Arial",40),
             fg="white",
-            font=("Arial",40,"bold")
+            bg="#1cc5d8"
         ).place(relx=0.5,rely=0.5,anchor="center")
-
-        right = tk.Frame(frame,bg="white",padx=60,pady=60)
+        right=tk.Frame(frame,bg="white",padx=40,pady=40)
         right.pack(side="right",expand=True)
         tk.Label(
             right,
             text="Đăng nhập",
-            font=("Arial",24,"bold"),
+            font=("Arial",22,"bold"),
             bg="white"
         ).pack(pady=20)
-        tk.Label(right,text="Email hoặc tên đăng nhập",bg="white").pack(anchor="w")
-        self.user = tk.Entry(right,width=35)
-        self.user.pack(pady=8)
+
+        tk.Label(right,text="Email",bg="white").pack(anchor="w")
+        self.entry_email=tk.Entry(right,width=35)
+        self.entry_email.pack(pady=5)
+
         tk.Label(right,text="Mật khẩu",bg="white").pack(anchor="w")
-        self.pw = tk.Entry(right,width=35,show="*")
-        self.pw.pack(pady=8)
+        self.entry_pass=tk.Entry(right,width=35,show="*")
+        self.entry_pass.pack(pady=5)
 
         tk.Button(
             right,
@@ -37,9 +42,31 @@ class DangNhapUI:
             width=20,
             command=self.login
         ).pack(pady=20)
-        tk.Label(right,text="Quên mật khẩu",fg="blue",bg="white").pack()
-        tk.Label(right,text="Chưa có tài khoản ? Đăng ký",bg="white").pack(pady=10)
+
+        tk.Button(
+            right,
+            text="Đăng ký",
+            command=self.open_register
+        ).pack()
+
     def login(self):
+        email=self.entry_email.get()
+        password=self.entry_pass.get()
+        try:
+            with open("users.json","r") as f:
+                data=json.load(f)
+        except:
+            messagebox.showerror("Lỗi","Chưa có tài khoản nào")
+            return
+        for user in data:
+            if user["email"]==email and user["password"]==password:
+                for w in self.root.winfo_children():
+                    w.destroy(
+                TrangChuUI(self.root,user["name"])
+                return
+        messagebox.showerror("Lỗi","Sai thông tin đăng nhập")
+    def open_register(self):
+
         for w in self.root.winfo_children():
             w.destroy()
-        TrangChuUI(self.root)
+        DangKyUI(self.root)
