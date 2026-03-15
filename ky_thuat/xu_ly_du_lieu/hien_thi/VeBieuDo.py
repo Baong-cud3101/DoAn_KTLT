e66a23a616de810c7d960889da6aad9e6bf73273
Baong-cud3101 <guenjiabao@gmai.com>
2026-03-16 02:44:42 +0700

    Bổ sung lại D:\Project_KTLT\ky_thuat\xu_ly_du_lieu\hien_thi\VeBieuDo.py

---
+++ ky_thuat/xu_ly_du_lieu/hien_thi/VeBieuDo.py
@@ -0,0 +1,4 @@
+class VeBieuDo:
+    def buildLineData(self, txList: list, period: str) -> dict: pass
+    def buildPieData(self, txList: list, category: str) -> dict: pass
+    def groupByMonth(self, txList: list) -> dict: pass
