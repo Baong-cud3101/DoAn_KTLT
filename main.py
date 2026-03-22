e3660efeac4c4af8889e4094668c5434111cd4a9
phucnmk25406h-lgtm <phucnmk25406h@st.uel.edu.vn>
2026-03-22 16:49:48 +0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import tkinter as tk
+from DangNhap import DangNhapUI
+
 def khoi_dong_he_thong():
-    pass
+    root = tk.Tk()
+    root.title("Finance App")
+    root.geometry("1000x600")
+    DangNhapUI(root)=
+    root.mainloop()
 
 if __name__ == "__main__":
     khoi_dong_he_thong()
