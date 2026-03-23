b1f02ed708709b11ef4f1946c83340f9cda4621a
phucnmk25406h-lgtm <phucnmk25406h@st.uel.edu.vn>
2026-03-23 20:23:54 +0700

    KiemTra.py

--- nghiep_vu/shared/KiemTra.py
+++ nghiep_vu/shared/KiemTra.py
@@ -1,3 +1,17 @@
 class KiemTraDuLieu:
     def kiem_tra_kieu_so(self, gia_tri) -> bool: pass
     def kiem_tra_rong(self, gia_tri) -> bool: pass
+
+    class KiemTraDuLieu:
+    def kiem_tra_kieu_so(self, gia_tri) -> bool:
+        try:
+            float(gia_tri)
+            return True
+        except:
+            return False
+    def kiem_tra_rong(self, gia_tri) -> bool:
+        if gia_tri is None:
+            return True
+        if isinstance(gia_tri, str) and gia_tri.strip() == "":
+            return True
+        return False
